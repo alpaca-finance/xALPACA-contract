74d0592fc9720f661f954ed00a4e9c0684442a95
sora-alpaca <sora.alpaca@gmail.com>
2021-11-09 12:54:58 +0700

    add fairlaunch interface

---
+++ contracts/8.7/interfaces/IFairLaunch.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+
+pragma solidity 0.8.7;
+
+interface IFairLaunch {
+  function pendingAlpaca(uint256 _pid, address _user) external view returns (uint256);
+
+  function deposit(
+    address _for,
+    uint256 _pid,
+    uint256 _amount
+  ) external;
+
+  function withdraw(
+    address _for,
+    uint256 _pid,
+    uint256 _amount
+  ) external;
+
+  function withdrawAll(address _for, uint256 _pid) external;
+
+  function harvest(uint256 _pid) external;
+}
