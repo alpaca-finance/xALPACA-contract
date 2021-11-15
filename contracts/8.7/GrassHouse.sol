9a70f4c7ab2d45b7535b430a06a8513e69120160
jr-alpaca <uviveramrom@gmail.com>
2021-11-15 13:52:23 +0700

    feat: make XALPACA and GrassHouse contracts upgradeable

--- contracts/8.7/GrassHouse.sol
+++ contracts/8.7/GrassHouse.sol
@@ -13,10 +13,11 @@ Alpaca Fin Corporation
 
 pragma solidity 0.8.7;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
-import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
 
 import "./interfaces/IxALPACA.sol";
 import "./interfaces/IBEP20.sol";
@@ -26,7 +27,7 @@ import "./SafeToken.sol";
 /// @title GrassHouse - Where Alpaca eats
 // solhint-disable not-rely-on-time
 // solhint-disable-next-line contract-name-camelcase
-contract GrassHouse is Ownable, ReentrancyGuard {
+contract GrassHouse is Initializable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
   using SafeToken for address;
 
   /// @dev Events
@@ -60,17 +61,20 @@ contract GrassHouse is Ownable, ReentrancyGuard {
   bool public isKilled;
   address public emergencyReturn;
 
-  /// @notice Constructor to instaniate GrassHouse
+  /// @notice Initialize GrassHouse
   /// @param _xALPACA The address of xALPACA
   /// @param _startTime Time to be started
   /// @param _rewardToken The token to be distributed
   /// @param _emergencyReturn The address to return token when emergency stop
-  constructor(
+  function initialize(
     address _xALPACA,
     uint256 _startTime,
     address _rewardToken,
     address _emergencyReturn
-  ) {
+  ) public initializer {
+    OwnableUpgradeable.__Ownable_init();
+    ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
+
     uint256 _startTimeFloorWeek = _timestampToFloorWeek(_startTime);
     startWeekCursor = _startTimeFloorWeek;
     lastTokenTimestamp = _startTimeFloorWeek;
@@ -96,11 +100,11 @@ contract GrassHouse is Ownable, ReentrancyGuard {
 
     uint256 _epoch = _findTimestampUserEpoch(_user, _timestamp, _maxUserEpoch);
     Point memory _point = IxALPACA(xALPACA).userPointHistory(_user, _epoch);
-    int128 _bias = _point.bias - _point.slope * SafeCast.toInt128(int256(_timestamp - _point.timestamp));
+    int128 _bias = _point.bias - _point.slope * SafeCastUpgradeable.toInt128(int256(_timestamp - _point.timestamp));
     if (_bias < 0) {
       return 0;
     }
-    return SafeCast.toUint256(_bias);
+    return SafeCastUpgradeable.toUint256(_bias);
   }
 
   /// @notice Record token distribution checkpoint
@@ -181,13 +185,13 @@ contract GrassHouse is Ownable, ReentrancyGuard {
         Point memory _point = IxALPACA(xALPACA).pointHistory(_epoch);
         int128 _timeDelta = 0;
         if (_weekCursor > _point.timestamp) {
-          _timeDelta = SafeCast.toInt128(int256(_weekCursor - _point.timestamp));
+          _timeDelta = SafeCastUpgradeable.toInt128(int256(_weekCursor - _point.timestamp));
         }
         int128 _bias = _point.bias - _point.slope * _timeDelta;
         if (_bias < 0) {
           totalSupplyAt[_weekCursor] = 0;
         } else {
-          totalSupplyAt[_weekCursor] = SafeCast.toUint256(_bias);
+          totalSupplyAt[_weekCursor] = SafeCastUpgradeable.toUint256(_bias);
         }
       }
       _weekCursor = _weekCursor + WEEK;
@@ -280,8 +284,11 @@ contract GrassHouse is Ownable, ReentrancyGuard {
           _userPoint = IxALPACA(xALPACA).userPointHistory(_user, _userEpoch);
         }
       } else {
-        int128 _timeDelta = SafeCast.toInt128(int256(_userWeekCursor - _prevUserPoint.timestamp));
-        uint256 _balanceOf = Math.max(SafeCast.toUint256(_prevUserPoint.bias - _timeDelta * _prevUserPoint.slope), 0);
+        int128 _timeDelta = SafeCastUpgradeable.toInt128(int256(_userWeekCursor - _prevUserPoint.timestamp));
+        uint256 _balanceOf = MathUpgradeable.max(
+          SafeCastUpgradeable.toUint256(_prevUserPoint.bias - _timeDelta * _prevUserPoint.slope),
+          0
+        );
         if (_balanceOf == 0 && _userEpoch > _maxUserEpoch) {
           break;
         }
@@ -295,7 +302,7 @@ contract GrassHouse is Ownable, ReentrancyGuard {
       }
     }
 
-    _userEpoch = Math.min(_maxUserEpoch, _userEpoch - 1);
+    _userEpoch = MathUpgradeable.min(_maxUserEpoch, _userEpoch - 1);
     userEpochOf[_user] = _userEpoch;
     weekCursorOf[_user] = _userWeekCursor;
 
