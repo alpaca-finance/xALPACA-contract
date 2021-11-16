0ee1a16e9d038a761983ea04c9317bf098413b8e
sora-alpaca <sora.alpaca@gmail.com>
2021-11-16 11:32:08 +0700

    refactor: clean test file

--- tests/TestMainnetFork.test.ts
+++ tests/TestMainnetFork.test.ts
@@ -4,10 +4,6 @@ import { solidity } from "ethereum-waffle";
 import {
   ProxyToken,
   ProxyToken__factory,
-  IFairLaunch__factory,
-  Timelock,
-  Timelock__factory,
-  IFairLaunch,
   GrassHouse,
   AlpacaFeeder,
   XALPACA,
@@ -17,10 +13,15 @@ import {
   GrassHouse__factory,
   AlpacaFeeder__factory,
 } from "../typechain";
+import {
+  Timelock,
+  Timelock__factory,
+  FairLaunch__factory,
+  FairLaunch,
+} from "@alpaca-finance/alpaca-contract/typechain";
 import * as timeHelpers from "./helpers/time";
 import { BigNumber, Signer } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { formatEther } from "@ethersproject/units";
 
 chai.use(solidity);
 const { expect } = chai;
@@ -31,7 +32,6 @@ const FAIR_LAUNCH = "0xA625AB01B08ce023B2a342Dbb12a16f2C8489A8F";
 const DEPLOYER = "0xC44f82b07Ab3E691F826951a6E335E1bC1bB0B51";
 const SHIELD = "0x1963f84395c8cf464e5483de7f2f434c3f1b4656";
 const ALPACA = "0x8F0528cE5eF7B51152A59745bEfDD91D97091d2F";
-const BNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 
 describe("AlpacaFeeder - Integration test", () => {
   let deployer: SignerWithAddress;
@@ -45,7 +45,7 @@ describe("AlpacaFeeder - Integration test", () => {
 
   // contracts
   let xalpaca: XALPACA;
-  let fairlaunch: IFairLaunch;
+  let fairlaunch: FairLaunch;
   let timelock: Timelock;
   let alpacaFeeder: AlpacaFeeder;
   let proxyToken: ProxyToken;
@@ -65,7 +65,7 @@ describe("AlpacaFeeder - Integration test", () => {
     // connect timelock
     timelock = await Timelock__factory.connect(TIME_LOCK, deployer);
     // connect fairlaunch
-    fairlaunch = await IFairLaunch__factory.connect(FAIR_LAUNCH, deployer);
+    fairlaunch = await FairLaunch__factory.connect(FAIR_LAUNCH, deployer);
 
     // console.log("Deploy xAlpaca");
     const XALPACA = (await ethers.getContractFactory("xALPACA", deployer)) as XALPACA__factory;
