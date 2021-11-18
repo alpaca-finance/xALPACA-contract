eb0f4a5e12bf93f05a5948860a27d55bee7d4305
black-alpacaa <black.alpacaa@gmail.com>
2021-11-18 18:11:26 +0700

    test: adding mdex worker at integration test AlpacaFeeder.

--- tests/integration/AlpacaFeederWithWorker.test.ts
+++ tests/integration/AlpacaFeederWithWorker.test.ts
@@ -21,6 +21,7 @@ import {
   MdexWorker02,
   CakeMaxiWorker02,
   CakeMaxiWorker02__factory,
+  MdexWorker02__factory,
 } from "@alpaca-finance/alpaca-contract/typechain";
 import * as timeHelpers from "../helpers/time";
 import { BigNumber, Signer } from "ethers";
@@ -35,10 +36,13 @@ const TIME_LOCK = "0x2D5408f2287BF9F9B05404794459a846651D0a59";
 const FAIR_LAUNCH = "0xA625AB01B08ce023B2a342Dbb12a16f2C8489A8F";
 const DEPLOYER = "0xC44f82b07Ab3E691F826951a6E335E1bC1bB0B51";
 const SHIELD = "0x1963f84395c8cf464e5483de7f2f434c3f1b4656";
-const WORKER = "0xecfB6E8BEceA9A65A5a367497230dF14F64A14C9";
+const CAKEMAXI_WORKER = "0xecfB6E8BEceA9A65A5a367497230dF14F64A14C9";
+const MDEX_WORKER = "0x5EffBF90F915B59cc967060740243037CE9E6a7E";
 const ALPACA = "0x8F0528cE5eF7B51152A59745bEfDD91D97091d2F";
 const WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 const CAKE = "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82";
+const MDEX = "0x9c65ab58d8d978db963e63f2bfb7121627e3a739";
+const BUSD = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
 
 describe("AlpacaFeeder - Integration test", () => {
   let deployer: SignerWithAddress;
@@ -56,6 +60,7 @@ describe("AlpacaFeeder - Integration test", () => {
   let proxyToken: ProxyToken;
   let grassHouse: GrassHouse;
   let worker: CakeMaxiWorker02;
+  let mdexWorker: MdexWorker02;
 
   async function fixture() {
     // const [bot, lyf] = await ethers.getSigners();
@@ -127,20 +132,26 @@ describe("AlpacaFeeder - Integration test", () => {
     await proxyToken.setOkHolders([alpacaFeeder.address, fairlaunch.address], true);
     await proxyToken.transferOwnership(alpacaFeeder.address);
 
-    worker = CakeMaxiWorker02__factory.connect(WORKER, deployer);
-
     const executeTime2 = (await timeHelpers.latestTimestamp())
       .add(timeHelpers.duration.days(BigNumber.from(2)))
       .toNumber();
+
+    worker = CakeMaxiWorker02__factory.connect(CAKEMAXI_WORKER, deployer);
+    mdexWorker = MdexWorker02__factory.connect(MDEX_WORKER, deployer);
+
+    // set reinvest whitelist
+    // CakeMaxiWorker
     await timelock.queueTransaction(
-      WORKER,
+      CAKEMAXI_WORKER,
       "0",
       "setReinvestorOk(address[],bool)",
       ethers.utils.defaultAbiCoder.encode(["address[]", "bool"], [[deployer.address], true]),
       executeTime2
     );
+    // set beneficial vault for worker
+    // CakeMaxiWorker
     await timelock.queueTransaction(
-      WORKER,
+      CAKEMAXI_WORKER,
       "0",
       "setBeneficialVaultConfig(uint256,address,address[])",
       ethers.utils.defaultAbiCoder.encode(
@@ -150,9 +161,13 @@ describe("AlpacaFeeder - Integration test", () => {
       executeTime2
     );
 
+    // advance time block
     await timeHelpers.increaseTimestamp(timeHelpers.duration.days(ethers.BigNumber.from(2)));
+
+    // execute set beneficial vault.
+    // CakeMaxi Worker
     await timelock.executeTransaction(
-      WORKER,
+      CAKEMAXI_WORKER,
       "0",
       "setBeneficialVaultConfig(uint256,address,address[])",
       ethers.utils.defaultAbiCoder.encode(
@@ -161,13 +176,20 @@ describe("AlpacaFeeder - Integration test", () => {
       ),
       executeTime2
     );
+    // Mdex
+    await mdexWorker.setBeneficialVaultConfig(100, alpacaFeeder.address, [MDEX, BUSD, ALPACA]);
+
+    // execute set reinvest whitelist
+    // CakeMaxiWorker
     await timelock.executeTransaction(
-      WORKER,
+      CAKEMAXI_WORKER,
       "0",
       "setReinvestorOk(address[],bool)",
       ethers.utils.defaultAbiCoder.encode(["address[]", "bool"], [[deployer.address], true]),
       executeTime2
     );
+    // Mdex
+    await mdexWorker.setReinvestorOk([deployer.address], true);
   }
 
   beforeEach(async () => {
@@ -175,13 +197,25 @@ describe("AlpacaFeeder - Integration test", () => {
   });
 
   describe("AlpacaFeeder", () => {
-    it("should work", async () => {
-      console.log("reinvest");
-      const balance = await alpaca.balanceOf(alpacaFeeder.address);
-      console.log(formatEther(balance.toString()));
-      await worker.reinvest();
-      console.log(formatEther(await (await alpaca.balanceOf(alpacaFeeder.address)).toString()));
-      expect(await alpaca.balanceOf(alpacaFeeder.address)).to.be.gt(balance);
+    context("when CakeMaxi worker is triggered reinvest", () => {
+      it("should send alpaca to AlpacaFeeder(CakeMaxi)", async () => {
+        console.log("reinvest CakeMaxi");
+        const balance = await alpaca.balanceOf(alpacaFeeder.address);
+        console.log(formatEther(balance.toString()));
+        await worker.reinvest();
+        console.log(formatEther(await (await alpaca.balanceOf(alpacaFeeder.address)).toString()));
+        expect(await alpaca.balanceOf(alpacaFeeder.address)).to.be.gt(balance);
+      });
+    });
+    context("when Mdex worker is triggered reinvest", () => {
+      it("should send alpaca to AlpacaFeeder(Mdex)", async () => {
+        console.log("reinvest mdex");
+        const balance = await alpaca.balanceOf(alpacaFeeder.address);
+        console.log(formatEther(balance.toString()));
+        await mdexWorker.reinvest();
+        console.log(formatEther(await (await alpaca.balanceOf(alpacaFeeder.address)).toString()));
+        expect(await alpaca.balanceOf(alpacaFeeder.address)).to.be.gt(balance);
+      });
     });
   });
 });
