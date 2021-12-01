bc281560fb985f9700cff0202db29f5f593c0e88
alpanaca <rblue4594@gmail.com>
2021-12-01 18:39:38 +0700

    feat: done contract

--- contracts/8.7/GrassHouseGateway.sol
+++ contracts/8.7/GrassHouseGateway.sol
@@ -17,17 +17,9 @@ import "./interfaces/IGrassHouse.sol";
 
 /// @title GrassHouseGateway
 contract GrassHouseGateway {
-  /// @notice Libraries
-
-  /// @notice Events
-
-  /// @notice State
-
-  /// @notice Attributes for Gateway
-
   function claimMultiple(address[] calldata _grassHouses, address _for) external {
     for (uint256 i = 0; i < _grassHouses.length; i++) {
-      IGrassHouse(_grassHouses[i]).claim();
+      IGrassHouse(_grassHouses[i]).claim(_for);
     }
   }
 }
--- contracts/8.7/interfaces/IGrassHouse.sol
+++ contracts/8.7/interfaces/IGrassHouse.sol
@@ -16,4 +16,5 @@ pragma solidity 0.8.7;
 interface IGrassHouse {
   function rewardToken() external returns (address);
   function feed(uint256 _amount) external returns (bool);
+  function claim(address _for) external returns (uint256);
 }
