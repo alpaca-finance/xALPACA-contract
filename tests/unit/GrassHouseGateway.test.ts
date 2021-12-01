bd50b5f12ee9b30d79d33cd7f06e3de23fb17239
alpanaca <rblue4594@gmail.com>
2021-12-01 18:39:51 +0700

    test: done unit test

--- tests/unit/GrassHouseGateway.test.ts
+++ tests/unit/GrassHouseGateway.test.ts
@@ -22,9 +22,13 @@ describe("AlpacaFeeder", () => {
 
   // Contact Instance
   let alpaca: BEP20;
+  let rewardToken: BEP20;
   let proxyToken: MockProxyToken;
 
   let grassHouse: MockGrassHouse;
+  let secondGrasshouse: MockGrassHouse;
+  let gateway: GrassHouseGateway;
+
 
   // Accounts
   let deployer: Signer;
@@ -34,7 +38,11 @@ describe("AlpacaFeeder", () => {
   let aliceAddress: string;
 
   // Contract Signer
-  let alpacaAsAlice: BEP20;
+  let alpacaAsDeployer: BEP20;
+
+  let rewardTokenAsDeployer: BEP20;
+
+  let gatewayAsAlice: GrassHouseGateway;
 
   async function fixture() {
     [deployer, alice] = await ethers.getSigners();
@@ -43,25 +51,73 @@ describe("AlpacaFeeder", () => {
     // Deploy ALPACA
     const BEP20 = (await ethers.getContractFactory("BEP20", deployer)) as BEP20__factory;
     alpaca = await BEP20.deploy("ALPACA", "ALPACA");
+    rewardToken = await BEP20.deploy("RTOKEN", "RTOKEN");
 
     // Deploy GrassHouse
     const MockGrassHouse = (await ethers.getContractFactory("MockGrassHouse", deployer)) as MockGrassHouse__factory;
     grassHouse = await MockGrassHouse.deploy(alpaca.address);
+    secondGrasshouse = await MockGrassHouse.deploy(rewardToken.address);
+
+    // Deploy Gateway
+    const Gateway = (await ethers.getContractFactory("GrassHouseGateway", deployer)) as GrassHouseGateway__factory;
+    gateway = await Gateway.deploy();
 
     // MINT
     await alpaca.mint(deployerAddress, ethers.utils.parseEther("8888888"));
-    await alpaca.mint(aliceAddress, ethers.utils.parseEther("8888888"));
+    await rewardToken.mint(deployerAddress, ethers.utils.parseEther("8888888"));
 
     // Assign contract signer
-    alpacaAsAlice = BEP20__factory.connect(alpaca.address, alice);
+    alpacaAsDeployer = BEP20__factory.connect(alpaca.address, deployer);
+    rewardTokenAsDeployer = BEP20__factory.connect(rewardToken.address, deployer);
+
+    gatewayAsAlice = GrassHouseGateway__factory.connect(gateway.address, alice);
+
+    // Set Mock Grasshouse to release 10 alpaca per claim
+    grassHouse.setRewardPerClaim(ethers.utils.parseEther("10"));
+    secondGrasshouse.setRewardPerClaim(ethers.utils.parseEther("20"));
+
+    // Transfer tokens to Grasshouse
+    alpacaAsDeployer.transfer(grassHouse.address, ethers.utils.parseEther("100"));
+    rewardTokenAsDeployer.transfer(secondGrasshouse.address, ethers.utils.parseEther("100"));
   }
 
   beforeEach(async () => {
     await waffle.loadFixture(fixture);
   });
 
-  describe("#initialize", () => {
-    it("should initialized correctly", async () => {
-    });
+  describe("#Claim", () => {
+    context("if all addresses are grasshouses", async () => {
+      it("should be able to claim through gateway", async () => {
+        expect(await alpaca.balanceOf(await alice.getAddress())).to.be.eq(0);
+
+        await gatewayAsAlice.claimMultiple([grassHouse.address, secondGrasshouse.address], await alice.getAddress());
+
+        expect(await alpaca.balanceOf(await alice.getAddress())).to.be.eq(ethers.utils.parseEther("10"));
+        expect(await rewardToken.balanceOf(await alice.getAddress())).to.be.eq(ethers.utils.parseEther("20"));
+      });
+    })
+
+    context("if some addresses are not grasshouses", async () => {
+      it("should revert", async () => {
+        expect(await alpaca.balanceOf(await alice.getAddress())).to.be.eq(0);
+
+        await expect(gatewayAsAlice.claimMultiple([grassHouse.address, alpaca.address], await alice.getAddress())).to.be.reverted;
+
+        
+        expect(await alpaca.balanceOf(await alice.getAddress())).to.be.eq(0);
+        expect(await rewardToken.balanceOf(await alice.getAddress())).to.be.eq(0);
+      });
+    })
+
+    context("if no addresses are provided", async () => {
+      it("should be ok", async () => {
+        expect(await alpaca.balanceOf(await alice.getAddress())).to.be.eq(0);
+
+        await expect(gatewayAsAlice.claimMultiple([], await alice.getAddress())).to.not.be.reverted;
+        
+        expect(await alpaca.balanceOf(await alice.getAddress())).to.be.eq(0);
+        expect(await rewardToken.balanceOf(await alice.getAddress())).to.be.eq(0);
+      });
+    })
   });
 });
