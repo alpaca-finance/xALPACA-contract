40c58740a11f21ad5e71cd75ebf516ec394450ae
black-alpacaa <black.alpacaa@gmail.com>
2021-12-13 16:29:58 +0700

    fix: remove SYMBOL const from deploy/proxy-token.ts

--- deploy/exec/proxy-token/deploy/proxy-token.ts
+++ deploy/exec/proxy-token/deploy/proxy-token.ts
@@ -14,13 +14,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
     Check all variables below before execute the deployment script
     */
-  const SYMBOL = "fdALPACA";
 
   const config = ConfigEntity.getConfig();
   const deployer = (await ethers.getSigners())[0];
 
-  console.log(`>> Deploying ${SYMBOL}`);
-  const ProxyToken = (await ethers.getContractFactory(SYMBOL, deployer)) as ProxyToken__factory;
+  console.log(`>> Deploying fdALPACA`);
+  const ProxyToken = (await ethers.getContractFactory("ProxyToken", deployer)) as ProxyToken__factory;
   const proxyToken = (await upgrades.deployProxy(ProxyToken, [`fdALPACA`, `fdALPACA`, config.Timelock])) as ProxyToken;
   await proxyToken.deployed();
   console.log(`>> Deployed at ${proxyToken.address}`);
