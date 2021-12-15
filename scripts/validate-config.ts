b9d52af3c060fd78d4054178568e37a3bfc71c0c
black-alpacaa <black.alpacaa@gmail.com>
2021-12-15 19:02:57 +0700

    test: prepare script for validate deployed contract and config.

--- scripts/validate-config.ts
+++ scripts/validate-config.ts
@@ -7,67 +7,128 @@ import TestnetAlpacaConfig from "@alpaca-finance/alpaca-contract/.testnet.json";
 import MainnetAlpacaConfig from "@alpaca-finance/alpaca-contract/.mainnet.json";
 import { WorkersEntity } from "../deploy/interfaces/config";
 import {
-  CakeMaxiWorker__factory,
+  CakeMaxiWorker02__factory,
   MdexWorker02__factory,
-  PancakeswapV2Worker__factory,
-  Vault,
+  PancakeswapV2Worker02__factory,
+  WorkerConfig__factory,
 } from "@alpaca-finance/alpaca-contract/typechain";
-
-interface IDexRouter {
-  pancakeswap: string;
-  waultswap: string;
-  mdex: string;
-}
 
 const config = ConfigEntity.getConfig();
 async function validateXAlpaca() {
   const xALPACA = XALPACA__factory.connect(config.xALPACA, ethers.provider);
-  expect(xALPACA.token).to.be.eq(config.Tokens.ALPACA);
+  expect(await xALPACA.token()).to.be.eq(config.Tokens.ALPACA);
 }
 
 async function validateProxyToken() {
+  console.log(`=== validate proxy token ===`);
   const proxyToken = ProxyToken__factory.connect(config.Tokens.fdALPACA, ethers.provider);
-  expect(proxyToken.timelock).to.be.eq(config.Timelock);
+  expect(await proxyToken.okHolders(config.FairLaunch.address)).to.be.eq(true);
+  expect(await proxyToken.okHolders(config.ALPACAFeeder)).to.be.eq(true);
 }
 
 async function validateGrassHouse() {
+  console.log(`=== validate grassHouse ===`);
   const grassHouses = config.GrassHouses;
   for (const grassHouse of grassHouses) {
     const grassHouseConnect = GrassHouse__factory.connect(grassHouse.address, ethers.provider);
-    expect(grassHouseConnect.rewardToken).to.be.eq(grassHouse.rewardToken);
+    expect(await grassHouseConnect.rewardToken()).to.be.eq(grassHouse.rewardToken);
   }
 }
 
 async function validateAlpacaFeeder() {
+  console.log(`=== validate AlpacaFeeder ===`);
   const alpacaFeeder = AlpacaFeeder__factory.connect(config.ALPACAFeeder, ethers.provider);
   const alpacaGrassHouse = config.GrassHouses.find((gh) => gh.rewardToken === config.Tokens.ALPACA);
   if (!alpacaGrassHouse) throw new Error(`Cannot find ALPACA GrassHouse`);
-  expect(alpacaFeeder.token()).to.be.eq(config.Tokens.ALPACA);
-  expect(alpacaFeeder.proxyToken()).to.be.eq(config.Tokens.fdALPACA);
-  expect(alpacaFeeder.fairLaunch()).to.be.eq(config.FairLaunch);
-  expect(alpacaFeeder.grassHouse()).to.be.eq(alpacaGrassHouse?.address);
+  expect(await alpacaFeeder.token()).to.be.eq(config.Tokens.ALPACA);
+  expect(await alpacaFeeder.proxyToken()).to.be.eq(config.Tokens.fdALPACA);
+  expect(await alpacaFeeder.fairLaunch()).to.be.eq(config.FairLaunch.address);
+  expect(await alpacaFeeder.grassHouse()).to.be.eq(alpacaGrassHouse?.address);
 }
 
-async function validateWorkersConfig(vault: Vault, workerInfo: WorkersEntity, routers: IDexRouter) {
+async function validateWorkersConfig(workerInfo: WorkersEntity) {
   console.log(`> validating ${workerInfo.name}`);
   if (workerInfo.name.includes("PancakeswapWorker")) {
-    const worker = PancakeswapV2Worker__factory.connect(workerInfo.address, ethers.provider);
-  } else if (workerInfo.name.includes("MdexWorker")) {
-    const worker = MdexWorker02__factory.connect(workerInfo.address, ethers.provider);
-  } else if (workerInfo.name.includes("CakeMaxiWorker")) {
-    const worker = CakeMaxiWorker__factory.connect(workerInfo.address, ethers.provider);
+    const worker = PancakeswapV2Worker02__factory.connect(workerInfo.address, ethers.provider);
+    try {
+      // expect(await worker.treasuryBountyBps()).to.be.eq("300");
+      expect(await worker.maxReinvestBountyBps()).to.be.eq("500");
+      // expect(await worker.reinvestBountyBps()).to.be.eq("300");
+      // expect(await worker.reinvestThreshold()).to.be.eq("1");
+      // expect(await worker.beneficialVaultBountyBps()).to.be.eq("5263");
+      // expect(await worker.beneficialVault()).to.be.eq(config.ALPACAFeeder);
+    } catch (e) {
+      console.log(`> ❌ some problem found in ${workerInfo.name}, please double check`);
+      console.log(e);
+    }
   }
+  // } else if (workerInfo.name.includes("MdexWorker")) {
+  //   const worker = MdexWorker02__factory.connect(workerInfo.address, ethers.provider);
+  //   try {
+  //     expect(await worker.treasuryBountyBps()).to.be.eq("900");
+  //     expect(await worker.maxReinvestBountyBps()).to.be.eq("900");
+  //     expect(await worker.reinvestBountyBps()).to.be.eq("300");
+  //     expect(await worker.reinvestThreshold()).to.be.eq("1");
+  //   } catch (e) {
+  //     console.log(`> ❌ some problem found in ${workerInfo.name}, please double check`);
+  //     console.log(e);
+  //   }
+  // } else if (workerInfo.name.includes("CakeMaxiWorker")) {
+  //   const worker = CakeMaxiWorker02__factory.connect(workerInfo.address, ethers.provider);
+  //   try {
+  //     expect(await worker.treasuryBountyBps()).to.be.eq("1900");
+  //     expect(await worker.beneficialVaultBountyBps()).to.be.eq("5263");
+  //     // expect(await worker.beneficialVault()).to.be.eq(config.ALPACAFeeder);
+  //   } catch (e) {
+  //     console.log(`> ❌ some problem found in ${workerInfo.name}, please double check`);
+  //     console.log(e);
+  //   }
+  // }
 }
 
 async function main() {
+  const configAlpaca = network.name === "mainnet" ? MainnetAlpacaConfig : TestnetAlpacaConfig;
   console.log("=== validate xALPACA ===");
   try {
-    await Promise.all([validateXAlpaca()]);
+    await Promise.all([validateXAlpaca(), validateProxyToken(), validateGrassHouse(), validateAlpacaFeeder()]);
     console.log("> ✅ Done");
   } catch (e) {
     console.log("> ❌ some problem found");
     console.log(e);
   }
+
+  const lpWorkerConfig = WorkerConfig__factory.connect(configAlpaca.SharedConfig.WorkerConfig, ethers.provider);
+  const cakeMaxiWorkerConfig = WorkerConfig__factory.connect(
+    configAlpaca.SharedConfig.PancakeswapSingleAssetWorkerConfig,
+    ethers.provider
+  );
+
+  const results: Array<WorkersEntity> = [];
+
+  for (let i = 0; i < configAlpaca.Vaults.length; i++) {
+    for (let j = 0; j < configAlpaca.Vaults[i].workers.length; j++) {
+      const worker = configAlpaca.Vaults[i].workers[j];
+      if (worker.name.indexOf("CakeMaxiWorker") === -1) {
+        const eachWorkerConfig = await lpWorkerConfig.workers(configAlpaca.Vaults[i].workers[j].address);
+        if (eachWorkerConfig.acceptDebt === false) {
+          continue;
+        }
+      } else {
+        const eachWorkerConfig = await cakeMaxiWorkerConfig.workers(configAlpaca.Vaults[i].workers[j].address);
+        if (eachWorkerConfig.acceptDebt === false) {
+          continue;
+        }
+      }
+
+      results.push(configAlpaca.Vaults[i].workers[j]);
+    }
+  }
+  const validateWorkers = [];
+  for (const worker of results) {
+    validateWorkers.push(validateWorkersConfig(worker));
+  }
+  await Promise.all(validateWorkers);
+  await delay(3000);
 }
 main()
   .then(() => process.exit(0))
@@ -75,3 +136,7 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
