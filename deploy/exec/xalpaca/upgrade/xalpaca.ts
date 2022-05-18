2b3cff0219fb45e768b01de68e619f1624c2afc5
abc-alpaca <gabcdef637@gmail.com>
2022-05-18 14:20:21 +0700

    feat: update script

--- deploy/exec/xalpaca/upgrade/xalpaca.ts
+++ deploy/exec/xalpaca/upgrade/xalpaca.ts
@@ -1,11 +1,10 @@
 import { XALPACA__factory } from "./../../../../typechain/factories/XALPACA__factory";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { ethers, upgrades } from "hardhat";
+import { ethers, upgrades, network } from "hardhat";
 import { getConfig } from "../../../entities/config";
-
 import { fileService, TimelockService } from "../../../services";
-import { getDeployer } from "../../../../utils/deployer-helper";
+import { getDeployer, isFork } from "../../../../utils/deployer-helper";
 import { compare } from "../../../../utils/address";
 import { ProxyAdmin__factory } from "@alpaca-finance/alpaca-contract/typechain";
 import { TimelockEntity } from "../../../entities";
@@ -30,7 +29,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const deployer = await getDeployer();
   console.log("> XALPACA ", TARGET_XALPACA_ADDRESS);
   console.log("> DEPLOYER ", deployer.address);
-  console.log("> Proxy Admin ", config.ProxyAdmin);
+  console.log("> PROXY ADMIN ", config.ProxyAdmin);
 
   const timelockTransactions: Array<TimelockEntity.Transaction> = [];
 
@@ -39,6 +38,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const newXALPACA = (await ethers.getContractFactory(NAME)) as XALPACA__factory;
   let nonce = await deployer.getTransactionCount();
   const preparedNewXALPACA = await upgrades.prepareUpgrade(TARGET_XALPACA_ADDRESS, newXALPACA);
+  const ops = isFork(network.name) ? { nonce: nonce++ } : { nonce: nonce++, gasLimit: 20000000 };
 
   if (isTimelockOwner) {
     console.log(`> Upgrading XALPACA at ${TARGET_XALPACA_ADDRESS} through Timelock + ProxyAdmin`);
@@ -54,17 +54,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         ["address", "address"],
         [TARGET_XALPACA_ADDRESS, preparedNewXALPACA],
         EXACT_ETA,
-        { nonce: nonce++ }
+        ops
       )
     );
   } else {
     console.log("> Executing without Timelock");
     console.log(`> Implementation address: ${preparedNewXALPACA}`);
     const proxyAdmin = ProxyAdmin__factory.connect(config.ProxyAdmin, deployer);
-    await proxyAdmin.upgrade(TARGET_XALPACA_ADDRESS, preparedNewXALPACA, {
-      nonce: nonce++,
-      gasLimit: 200000000,
-    });
+    await proxyAdmin.upgrade(TARGET_XALPACA_ADDRESS, preparedNewXALPACA, ops);
 
     console.log("✅ Done");
   }
