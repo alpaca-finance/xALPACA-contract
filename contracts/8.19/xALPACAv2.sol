ddf9910f9015e309f8d9b7b04a8712d93afe58b6
alpanaca <rblue4594@gmail.com>
2023-09-22 16:45:10 +0700

    feat: add breaker

--- contracts/8.19/xALPACAv2.sol
+++ contracts/8.19/xALPACAv2.sol
@@ -24,12 +24,17 @@ import { SafeToken } from "./SafeToken.sol";
 // solhint-disable not-rely-on-time
 // solhint-disable-next-line contract-name-camelcase
 contract xALPACAv2 is ReentrancyGuardUpgradeable, OwnableUpgradeable {
+  using SafeToken for address;
+
+  //--------- Errors ------------//
   error xALPACAv2_InvalidAmount();
   error xALPACAv2_InvalidStatus();
   error xALPACAv2_UnlockTimeUnreached();
 
-  using SafeToken for address;
+  //--------- Events ------------//
+  event LogSetBreaker(uint256 _previousBreaker, uint256 _breaker);
 
+  //--------- States ------------//
   // Token to be locked (ALPACA)
   address public token;
 
@@ -38,6 +43,9 @@ contract xALPACAv2 is ReentrancyGuardUpgradeable, OwnableUpgradeable {
 
   // Total amount of token locked
   uint256 public totalLocked;
+
+  // Flag to allow emergency withdraw
+  uint256 public breaker;
 
   mapping(address => uint256) public userLockAmounts;
 
@@ -93,16 +101,17 @@ contract xALPACAv2 is ReentrancyGuardUpgradeable, OwnableUpgradeable {
       status: 0
     });
 
-    UnlockRequest[] storage _userRequests = userUnlockRequests[msg.sender];
+    if (breaker == 0) {
+      UnlockRequest[] storage _userRequests = userUnlockRequests[msg.sender];
 
-    uint256 _requestId = _userRequests.length;
+      _unlockRequestId = _userRequests.length;
 
-    _userRequests.push(_request);
-
-    // interaction
-    // todo: withdraw from miniFL
-
-    return _requestId;
+      _userRequests.push(_request);
+      // interaction
+      // todo: withdraw from miniFL
+    } else {
+      token.safeTransfer(msg.sender, _amount);
+    }
   }
 
   /// @dev Claim the unlocked ALPACA
@@ -127,9 +136,6 @@ contract xALPACAv2 is ReentrancyGuardUpgradeable, OwnableUpgradeable {
     token.safeTransfer(msg.sender, request.amount);
   }
 
-  /// @dev Withdraw without delayed unlocking
-  function emergencyWithdraw(uint256 _amount) external {}
-
   /// @dev Reverse the withdrawal unlocking process
   function cancelUnlock(uint256 _unlockRequestId) external {
     UnlockRequest storage request = userUnlockRequests[msg.sender][_unlockRequestId];
@@ -149,10 +155,23 @@ contract xALPACAv2 is ReentrancyGuardUpgradeable, OwnableUpgradeable {
     // todo: deposit back to miniFL
   }
 
+  // -------- Privilege Functions -----//
+
   /// @dev Owner set the delayed unlock time
   function setDelayUnlockTime(uint256 _newDelayUnlockTime) external onlyOwner {
     delayUnlockTime = _newDelayUnlockTime;
   }
+
+  /// @dev Owner enable emergency withdraw
+  /// @param _breaker The new value of breaker 0 if off, 1 if on
+  function setBreaker(uint256 _breaker) external onlyOwner {
+    require(_breaker == 0 || _breaker == 1, "only 0 or 1");
+    uint256 _previousBreaker = breaker;
+    breaker = _breaker;
+    emit LogSetBreaker(_previousBreaker, breaker);
+  }
+
+  // -------- View Functions --------//
 
   function userUnlockRequestsLastId(address _user) external view returns (uint256) {
     return userUnlockRequests[_user].length;
