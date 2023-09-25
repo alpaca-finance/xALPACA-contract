8a9ce9b371921daf4b243d1da05299d056f6a08e
alpanaca <rblue4594@gmail.com>
2023-09-25 19:20:32 +0700

    test: add revert test case

--- tests/forge/xALPACAv2/xALPACAv2_WIthdrawReserve.t.sol
+++ tests/forge/xALPACAv2/xALPACAv2_WIthdrawReserve.t.sol
@@ -38,4 +38,25 @@ contract xALPACAv2_WithdrawReserveTest is BaseTest {
 
     assertEq(alpaca.balanceOf(address(this)), _feeReserve);
   }
+
+  function testRevert_WhenOwnerWithdrawReserveMoreThanAvailable_ShouldRevert() external {
+    uint256 _lockAmount = 10 ether;
+    uint256 _unlockAmount = 4 ether;
+    // 50 bps per day
+    xALPACA.setEarlyWithdrawFeeBpsPerDay(50);
+
+    vm.startPrank(ALICE);
+    alpaca.approve(address(xALPACA), type(uint256).max);
+    xALPACA.lock(_lockAmount);
+
+    uint256 _unlockId = xALPACA.unlock(_unlockAmount);
+
+    xALPACA.earlyWithdraw(_unlockId);
+    vm.stopPrank();
+
+    uint256 _feeReserve = xALPACA.feeReserve();
+    xALPACA.withdrawReserve(address(this), _feeReserve);
+    vm.expectRevert(abi.encodeWithSelector(xALPACAv2.xALPACAv2_InvalidAmount.selector));
+    xALPACA.withdrawReserve(address(this), 1);
+  }
 }
