79bba1e595b461d490246d8a14748fbf879b4482
jr-alpaca <uviveramrom@gmail.com>
2023-10-15 16:33:51 +0700

    fix: alpaca migrator and add test case

--- tests/forge/xALPACAMigrator/xALPACAMigrator_MigrateToV2Test.t.sol
+++ tests/forge/xALPACAMigrator/xALPACAMigrator_MigrateToV2Test.t.sol
@@ -16,11 +16,19 @@ contract xALPACAMigrator_MigrateToV2Test is xALPACAMigrator_BaseTest {
     vm.startPrank(deployer);
     xalpacaMigrator.setxALPACAv2(address(xAlpacaV2));
 
+    uint256 userPointEpochBefore = xalpacaMigrator.userPointEpoch(CAT);
+
     address[] memory _users = new address[](1);
     _users[0] = CAT;
     xalpacaMigrator.migrateToV2(_users);
 
     vm.stopPrank();
+
+    (int128 _catLockedAmountAfter, uint256 _catEndAfter) = xalpacaMigrator.locks(CAT);
+
+    assertEq(_catLockedAmountAfter, 0);
+    assertEq(_catEndAfter, 0);
+    assertEq(xalpacaMigrator.userPointEpoch(CAT), userPointEpochBefore);
 
     assertEq(xAlpacaV2.userLockAmounts(CAT), 0);
     assertEq(xAlpacaV2.totalLocked(), 0);
@@ -30,6 +38,8 @@ contract xALPACAMigrator_MigrateToV2Test is xALPACAMigrator_BaseTest {
     // warp timestamp so that alice and bob locks expired
     vm.warp(block.timestamp + 3 weeks);
 
+    uint256 _alicePointEpochBefore = xalpacaMigrator.userPointEpoch(ALICE);
+    uint256 _bobPointEpochBefore = xalpacaMigrator.userPointEpoch(BOB);
     vm.startPrank(deployer);
     xalpacaMigrator.setxALPACAv2(address(xAlpacaV2));
 
@@ -40,8 +50,21 @@ contract xALPACAMigrator_MigrateToV2Test is xALPACAMigrator_BaseTest {
 
     vm.stopPrank();
 
+    (int128 _aliceLockedAmountAfter, uint256 _aliceEndAfter) = xalpacaMigrator.locks(ALICE);
+    (int128 _bobLockedAmountAfter, uint256 _bobEndAfter) = xalpacaMigrator.locks(BOB);
+
+    // assert alice
+    assert(_aliceEndAfter != 0);
+    assertEq(_aliceLockedAmountAfter, 10 ether);
+    assertEq(xalpacaMigrator.userPointEpoch(ALICE), _alicePointEpochBefore);
     assertEq(xAlpacaV2.userLockAmounts(ALICE), 0);
+
+    // assert bob
+    assert(_bobEndAfter != 0);
+    assertEq(_bobLockedAmountAfter, 20 ether);
+    assertEq(xalpacaMigrator.userPointEpoch(BOB), _bobPointEpochBefore);
     assertEq(xAlpacaV2.userLockAmounts(BOB), 0);
+
     assertEq(xAlpacaV2.totalLocked(), 0);
   }
 
@@ -49,6 +72,9 @@ contract xALPACAMigrator_MigrateToV2Test is xALPACAMigrator_BaseTest {
     vm.startPrank(deployer);
     xalpacaMigrator.setxALPACAv2(address(xAlpacaV2));
 
+    uint256 _alicePointEpochBefore = xalpacaMigrator.userPointEpoch(ALICE);
+    uint256 _bobPointEpochBefore = xalpacaMigrator.userPointEpoch(BOB);
+
     address[] memory _users = new address[](2);
     _users[0] = ALICE;
     _users[1] = BOB;
@@ -56,8 +82,24 @@ contract xALPACAMigrator_MigrateToV2Test is xALPACAMigrator_BaseTest {
 
     vm.stopPrank();
 
+    (int128 _aliceLockedAmountAfter, uint256 _aliceEndAfter) = xalpacaMigrator.locks(ALICE);
+    (int128 _bobLockedAmountAfter, uint256 _bobEndAfter) = xalpacaMigrator.locks(BOB);
+
+    // assert alice
+    assertEq(_aliceLockedAmountAfter, 0);
+
+    assertEq(_aliceEndAfter, 0);
+    assertEq(xalpacaMigrator.userPointEpoch(ALICE), _alicePointEpochBefore + 1);
+    assertEq(xalpacaMigrator.balanceOf(ALICE), 0);
     assertEq(xAlpacaV2.userLockAmounts(ALICE), aliceLockAmount);
+
+    // assert bob
+    assertEq(_bobLockedAmountAfter, 0);
+    assertEq(_bobEndAfter, 0);
+    assertEq(xalpacaMigrator.userPointEpoch(BOB), _bobPointEpochBefore + 1);
+    assertEq(xalpacaMigrator.balanceOf(BOB), 0);
     assertEq(xAlpacaV2.userLockAmounts(BOB), bobLockAmount);
+
     assertEq(xAlpacaV2.totalLocked(), aliceLockAmount + bobLockAmount);
   }
 }
