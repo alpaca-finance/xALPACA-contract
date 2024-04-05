bc8c103fe146f30f85172c1494c3d992498600e7
alpanaca <rblue4594@gmail.com>
2024-04-05 14:59:23 +0700

    fix: comment

--- contracts/8.19/xALPACAv2RevenueDistributor/xALPACAv2Rewarder.sol
+++ contracts/8.19/xALPACAv2RevenueDistributor/xALPACAv2Rewarder.sol
@@ -109,11 +109,11 @@ contract xALPACAv2Rewarder is IxALPACAv2Rewarder, OwnableUpgradeable, Reentrancy
     //  - accRewardPerShare    = 250
     //  - _receivedAmount      = 100
     //  - pendingRewardReward  = 25,000
-    //  rewardDebt = previousRewardDebt + (_receivedAmount * accRewardPerShare)= 100 + (100 * 250) = 26,000
-    //  This means newly deposit share does not eligible for 26,000 pending rewards
+    //  rewardDebt = previousRewardDebt + (_receivedAmount * accRewardPerShare)= 100 + (100 * 250) = 25,100
+    //  This means newly deposit share does not eligible for 25,100 pending rewards
 
     // handle if user already has deposited
-    // reward users supposed to get will be accounted here
+    // reward users supposed to get will be accounted here first
     if (user.rewardDebt == 0 && _previousAmount > 0) {
       user.rewardDebt =
         user.rewardDebt -
