79cd9947b8d07a62f15fc3fe6c1cae3e18126e17
alpanaca <rblue4594@gmail.com>
2024-04-05 14:51:38 +0700

    chore: fix amount

--- deploy/exec/xalpaca-v2-rewarder/config/withdraw.ts
+++ deploy/exec/xalpaca-v2-rewarder/config/withdraw.ts
@@ -24,7 +24,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const REWARDERS: Array<IRewarder> = [
     {
       NAME: "PYTH",
-      AMOUNT: "1",
+      AMOUNT: "175.26",
       DECIMAL: 6,
     },
   ];
